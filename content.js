ed2b33e59478f6b560fe472ec1b618ec1c373b2c
Tanner Leslie <tleslie@westmont.edu>
2020-04-06 17:49:46 -0700

    Attempted to get a server going

--- content.js
+++ content.js
@@ -1,14 +1,31 @@
-//const firstHref = $("a[href^='http']").eq(0).attr("href");
-//alert(firstHref);
+/*
+const socket = new WebSocket('ws://localhost:8080');
 
+socket.addEventListener('open', function (event) {
+    socket.send('Hello Server!');
+});
+
+socket.addEventListener('message', function (event) {
+    console.log('Message from server ', event.data);
+});
+*/
+var io = require('socket.io');
+
+var socket = io.connect();
+
+socket.on('log', function(array){
+  console.log.apply(console,array);
+});
 
 $(document.documentElement).on('click', function(event){
-  alert('this works');
+  console.log('this works - console');
+  socket.send('this works - socket');
   let $store = $(event.currentTarget); //this needs to be passed on to the other user
   //then $store.trigger('click') will force the browser to make a click
   //something must be put in place to stop an infinite loop occuring
   //EX: on user clicks something and that is sent to the other user who 'clicks'
   //the same thing and so that click is sent back to the first user and so on
   //returning false stops the event from occuring, returning true allows it to happen
+
   return true;
 });
