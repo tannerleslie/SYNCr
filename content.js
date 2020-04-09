74254be57e3c2be0045ac5407d85b132d5dbb5ba
Tanner Leslie <tleslie@westmont.edu>
2020-04-08 18:41:56 -0700

    Got a server up and running

--- content.js
+++ content.js
@@ -1,4 +1,4 @@
-/*
+
 const socket = new WebSocket('ws://localhost:8080');
 
 socket.addEventListener('open', function (event) {
@@ -6,26 +6,19 @@ socket.addEventListener('open', function (event) {
 });
 
 socket.addEventListener('message', function (event) {
-    console.log('Message from server ', event.data);
-});
-*/
-var io = require('socket.io');
-
-var socket = io.connect();
-
-socket.on('log', function(array){
-  console.log.apply(console,array);
+    //console.log('Message from server ', JSON.stringify(event.data));
+    //$(event.data).hide(); doesn't work
 });
 
-$(document.documentElement).on('click', function(event){
-  console.log('this works - console');
-  socket.send('this works - socket');
-  let $store = $(event.currentTarget); //this needs to be passed on to the other user
+$('*').on('click', function(event){
+  console.log(JSON.stringify($(event.currentTarget).get()));
+  let store = $(event.currentTarget); //this needs to be passed on to the other user
   //then $store.trigger('click') will force the browser to make a click
   //something must be put in place to stop an infinite loop occuring
   //EX: on user clicks something and that is sent to the other user who 'clicks'
   //the same thing and so that click is sent back to the first user and so on
   //returning false stops the event from occuring, returning true allows it to happen
-
-  return true;
+  socket.send(store);
+  //$(event.currentTarget).hide();
+  return false;
 });
