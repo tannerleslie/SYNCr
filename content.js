769a348aa8e8b38ebb9a3b520aef3a57a2a2edb7
Tanner Leslie <tleslie@westmont.edu>
2020-05-01 10:13:00 -0700

    Completed project

--- content.js
+++ content.js
@@ -1,24 +1,16 @@
 
 const socket = new WebSocket('ws://localhost:8080');
 
-socket.addEventListener('open', function (event) {
-    socket.send('Hello Server!');
-});
-
 socket.addEventListener('message', function (event) {
-    //console.log('Message from server ', JSON.stringify(event.data));
-    //$(event.data).hide(); doesn't work
+    if(event.data !== 'undefined'){
+      console.log('Redirecting to ', event.data);
+      window.location.href = event.data;
+    }
 });
 
 $('*').on('click', function(event){
-  console.log(JSON.stringify($(event.currentTarget).get()));
-  let store = $(event.currentTarget); //this needs to be passed on to the other user
-  //then $store.trigger('click') will force the browser to make a click
-  //something must be put in place to stop an infinite loop occuring
-  //EX: on user clicks something and that is sent to the other user who 'clicks'
-  //the same thing and so that click is sent back to the first user and so on
-  //returning false stops the event from occuring, returning true allows it to happen
+  console.log('click registered');
+  let store = $(event.currentTarget).attr('href');
   socket.send(store);
-  //$(event.currentTarget).hide();
   return false;
 });
--- server.js
+++ server.js
@@ -6,8 +6,11 @@ const app = express();
 
 const server = new WebSocket.Server({ server: app.listen(8080) });
 
+console.log('the server is running');
+
 server.on('connection', socket => {
   socket.on('message', message => {
+    console.log(message);
     server.clients.forEach(client => {
       client.send(message);
     });
